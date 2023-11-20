c98385043defadabe34c2534df9c97ef26816c8d
dahye <bbngsk@gmail.com>
2023-11-20 11:46:34 +0900

    Initial Commit

---
+++ CoinOrderbook/CoinOrderbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinOrderbookApp.swift
+//  CoinOrderbook
+//
+//  Created by 박다혜 on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinOrderbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinOrderbook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinOrderbook
+//
+//  Created by 박다혜 on 11/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
